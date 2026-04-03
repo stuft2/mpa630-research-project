74ebcb7c686ee1ba6de873bcd85bf16fdfd90ff2
Spencer Tuft <spencer@tuft.cloud>
2026-04-02 20:10:01 -0600

    add robustness checks

--- regression.R
+++ regression.R
@@ -4,8 +4,6 @@
 #Date: 04/2026
 #####----------------------------------------------#####
 
-p_load(dplyr, readr, fixest)
-
 # Load cleaned ANES data
 dt <- read_csv("anes_all_cleaned_vars.csv", show_col_types = FALSE)
 
@@ -33,11 +31,28 @@ logit <- feglm(
   vcov = "hetero"
 )
 
-summary(logit)
+summary(logit, vcov = "hetero")
 
-# 3. Controls summary for variables of interest
-controls_summary <- dt |>
-  select(age_clean, sex, party, state_clean) |>
-  summary()
+# Transform the bachelor's degree logit coefficient into a percent change in odds
+b <- coef(logit)["educationBachelor's degree"]
+exp_b <- exp(b)
+pct_change <- (exp_b - 1) * 100
 
-print(controls_summary)
+# 3. Robustness checks
+
+# a) Collinearity
+check_collinearity(lpm)
+
+# b) Change of controls
+dt <- dt |>
+  mutate(
+    marital = relevel(factor(marital_simple), ref = "Married")
+  )
+
+controls <- feols(
+  trust_binary ~ income + race + education + sex + age_clean + marital + party,
+  data = dt,
+  vcov = "hetero"
+)
+
+summary(controls)
